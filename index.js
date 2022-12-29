58cfb0ae7c8bf2fe57f48d4242b83ffb7bd2926a
hudoznik <hudoznik-1@ya.ru>
2022-12-30 00:11:09 +0300

    Готовит sql файл для загрузки в базу

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const Excel = require('exceljs');
+const fs = require('fs');
+const table = 'contragent';
+const newLineChar = require('os').EOL;
+fs.appendFileSync('insert.sql', `${newLineChar}`);
 const workbook = new Excel.Workbook();
 let main = async () => {
-        await workbook.xlsx.readFile('/home/hudoznik/Загрузки/Telegram Desktop/для MySQL WB.xlsx');
+        await workbook.xlsx.readFile('./для MySQL WB.xlsx');
         let worksheet = workbook.getWorksheet();
         let _rows = worksheet._rows;
-        let colums = [];
+        let colums = {qqqqqqqqqqqqq: 'market'};
         _rows[0]._cells.forEach((el) => {
-                        colums[el._address] = el.value
+                colums[el._address.replace(/[0-9]/,'')] = el.value
         })
+        let rows = [];
         debugger;
         _rows.forEach((row,i) => {
                 if (i == 0) return
-                colums.forEach((c,i) => {
-                        let rc = row._cells.find(rc => rc._address == i)
+                if (row._cells.length == 0) return;
+                // console.log(row)
+                // let obj = {};
+                let obj = ['wildberries'];
+                Object.keys(colums).forEach((c) => {
+                        if (c == 'qqqqqqqqqqqqq') return
+                        let rc = row._cells.find(rc => {
+                                if (rc == undefined) return false
+                                console.log(c)
+                                return rc._address.replace(/[0-9]/,'') == c
+                        })
                         console.log(c, rc)
+                        // obj[colums[c]] = (rc) ? rc.value : '';
+                        obj.push((rc) ? rc.value : '');
                 })
+                rows.push('("'+obj.join('","')+'")')
             // console.log(i)
         })
         debugger;
+        await fs.writeFileSync('insert.sql', `INSERT INTO ${table} (${ Object.keys(colums).reduce((map,el) => {
+                map.push(colums[el])
+                return map
+        },[]).join(',')}) VALUES`)
+        await fs.appendFileSync('insert.sql', `${newLineChar}${rows.join(',')};`);
+
+        
         process.exit();
 }
 main()
